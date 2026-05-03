0b07f9ff73e7e8811b6cf4a7f5cfd30a82df84df
tabasumsunny2-ops <tabasumsunny2@gmail.com>
2026-05-03 23:47:46 +0500

    Update index.js

--- index.js
+++ index.js
@@ -43,10 +43,10 @@ console.log(JSON.stringify(req.body, null, 2));
                     const from = msg.from;
 
                     let text = "";
-                    if (msg.text && msg.text.body) {
+
+if (msg.text && msg.text.body) {
     text = msg.text.body.toLowerCase();
 }
-                    }
 
                     let reply = "";
 
